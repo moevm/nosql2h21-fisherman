a74741410b8bcd34326b85a6e0fe692cf754865d
AlexanderShchuka <Shuka.sashaa@gmail.com>
2021-09-12 16:53:10 +0300

    Connect DB with Fronend and Backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/test');
+const Hello = mongoose.model("Hello", {name: String});
 
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
-    next();
+});
+
+router.get('/test', function (req, res, next) {
+    let answer = helloWorld();
+    res.json(answer);
 });
 
 router.get('*', (req, res) => {
     res.status(404);
     res.end();
 });
+
+function helloWorld() {
+    const hello = new Hello({name: "World"});
+    hello.save();
+    return hello.toJSON();
+}
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,34 @@
 import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 import {Button} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button>Hello World</Button>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        data: "READY"
+    };
+
+    handler = () => {
+        fetch("http://localhost:8080/test")
+            .then(res => res.json())
+            .then(json => {
+                if (json) {
+                    this.setState({data: "DONE"});
+                }
+            })
+    };
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <Button onClick={this.handler}>Hello World</Button>
+                    <p>{this.state.data}</p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
