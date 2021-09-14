02b29f507c2586207418425051d3e6594ed52881
Мельникова Ольга <42916312+olga-alexandrovnaa@users.noreply.github.com>
2021-09-14 20:23:26 +0300

    mobx update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,26 @@
 import logo from './logo.svg';
 import React, {Component} from "react";
+import { observer } from "mobx-react-lite";
 import './App.css';
 import {Button} from "react-bootstrap";
+import HelloWorld from "./store/helloWorldStore.js"
+import ReactDOM from "react-dom";
 
+const App = observer(() => {
 
-class App extends Component {
-    state = {
-        data: "READY"
-    };
-
-    handler = () => {
-        fetch("http://localhost:8080/test")
-            .then(res => res.json())
-            .then(json => {
-                if (json) {
-                    this.setState({data: "DONE"});
-                }
-            })
-    };
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <Button onClick={this.handler}>Hello World</Button>
-                    <p>{this.state.data}</p>
-                </header>
-            </div>
-        );
-    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <Button onClick={()=>{HelloWorld.get()}}>Hello World</Button>
+                <p>{HelloWorld.data}</p>
+            </header>
+        </div>
+    );
+    
 }
+)
 
 export default App;
+
+
