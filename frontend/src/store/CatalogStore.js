3107730e1689ad6e7202da8ada159248799758b7
Мельникова Ольга <42916312+olga-alexandrovnaa@users.noreply.github.com>
2021-11-24 16:34:19 +0300

    Update CatalogStore.js

--- frontend/src/store/CatalogStore.js
+++ frontend/src/store/CatalogStore.js
@@ -48,6 +48,23 @@ class Catalog {
     });
   }
 
+  getProduct = (id) =>{
+    console.log(id)
+    let res = {
+      image: '',
+      count: 0,
+      title: '',
+      price: 0
+    }
+    for (const element of  toJS(this.array)) {
+      if(element._id === id){
+        res = element
+      }
+    }
+    console.log(res)
+    return res
+  }
+
   get = async () => {
     this.isLoading = true;
     return await fetch('http://localhost:8080/products').then( async res => { 
@@ -57,23 +74,45 @@ class Catalog {
 
   };
 
-  add = async () => {
+  add = async (imageAdd, vendorCodeAdd, titleAdd, descriptionAdd, priceAdd, countAdd) => {
     this.isLoading = true;
-    return await fetch('http://localhost:8080/products').then( async res => { 
-      this.array = await res.json();
+    return await fetch('http://localhost:8080/products/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({
+        "image": imageAdd,
+        "vendorCode": vendorCodeAdd,
+        "title": titleAdd,
+        "description": descriptionAdd,
+        "price": priceAdd,
+        "count": countAdd,
+      })
+    }).then( async res => { 
       this.isLoading = false;
+      return await res.json();
     }).catch((e) => console.log(e.message));
 
   };
 
   delete = async (id) => {
     this.isLoading = true;
-    return await fetch('http://localhost:8080/products').then( async res => { 
-      this.array = await res.json();
+    return await fetch('http://localhost:8080/products/delete', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({
+        "id": id,
+      })
+    }).then( async res => { 
       this.isLoading = false;
+      return await res.json();
     }).catch((e) => console.log(e.message));
 
   };
 
 }
 export default new Catalog();
+
