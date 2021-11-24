95eb3ef5b926898f2e56e5b6b4806b82e66428d9
Мельникова Ольга <42916312+olga-alexandrovnaa@users.noreply.github.com>
2021-11-24 16:38:12 +0300

    Update Cart.js

--- frontend/src/Components/Cart.js
+++ frontend/src/Components/Cart.js
@@ -2,17 +2,22 @@ import React from "react";
 import {Route, Switch} from "react-router-dom";
 import {Button, Col, Container, FormControl, Jumbotron, Row} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
+import UserStore from "../store/UserStore"
 
 const Cart = observer((state) => {
     console.log("in",state)
     return (
         <div className="card m-3" style={{width: "18rem"}}>
-            <img className="card-img-top" src={state.data.img} alt="Card image cap"/>
+            <img className="card-img-top" src={state.data.image} alt="Card image cap"/>
             <div className="card-body">
-                <p className="card-text text-center">{state.data.name}</p>
-                <h6 className="card-title text-center">{state.data.cost + " руб."}</h6>
+            <h5 className="card-title text-center">{state.data.title}</h5>
+                <p className="card-text text-center">{state.data.description}</p>
+                <h6 className="card-title text-center">{state.data.price + " руб."}</h6>
                 <div class="col text-center">
-                    <a href="#" id={state.data.id} className="btn btn-primary ">Купить</a>
+                    <a href="#" id={state.data._id} className="btn btn-primary "
+                    onClick={(e)=>{
+                        UserStore.pushCart( {id: e.target.id, count: 1 })
+                    }}>Купить</a>
                 </div>
                 
             </div>
