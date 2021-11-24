48391f4f4a77efb558a32befa4fd246d91e1a833
Мельникова Ольга <42916312+olga-alexandrovnaa@users.noreply.github.com>
2021-11-24 16:39:56 +0300

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,11 +11,8 @@ import OrdersStore from "../store/OrdersStore"
 import { toJS } from "mobx";
 
 const Home = observer(() => {
-    CatalogStore.get()
-    OrdersStore.get()
 
-    const [cart, SetCart] = useState([])//корзина
-
+    
     //для авторизации\регистрации
     const [fio, fioSetValue] = useState(null);
     const [login, loginSetValue] = useState(null);
@@ -30,13 +27,12 @@ const Home = observer(() => {
     const [descriptionAdd, descriptionAddSetValue] = useState(null);
     const [priceAdd, priceAddSetValue] = useState(null); 
     const [countAdd, countAddSetValue] = useState(null);
-    
+
     //добавление заказа
     const [fioAddOrder, fioAddOrderSetValue] = useState(null);
     const [phoneAddOrder, phoneAddOrderSetValue] = useState(null);
     const [addressAddOrder, addressAddOrderSetValue] = useState(null);
     const [commentAddOrder, commentAddOrderSetValue] = useState(null);
-
 
     //сортировка   
     const [searchVal, searchValSetValue] = useState(null);
@@ -56,10 +52,33 @@ const Home = observer(() => {
         console.log("onAuth")
         CatalogStore.add(imageAdd, vendorCodeAdd, titleAdd, descriptionAdd, priceAdd, countAdd)
     }
+     //модальное окно просмотра корзины
+     const [isModalCart, setModalCart] = React.useState(false)
+     const onCloseCart = () => {
+         setModalCart(false)
+     }
+     const ArrangeOrder = async() => {
+        let number = await OrdersStore.add({
+            "products": UserStore.cart,
+            "fio": fioAddOrder,
+            "phone": phoneAddOrder,
+            "address": addressAddOrder,
+            "comment": commentAddOrder
+        })
+        setModalOrderNumber(number ? number : '')
+        onCloseCart()
+        setModalThankYou(true)
+     }
+    //модальное окно благодарство
+    const [OrderNumber, setModalOrderNumber] = React.useState(false)
+      const [isModalThankYou, setModalThankYou] = React.useState(false)
+      const onCloseThankYou = () => {
+          setModalThankYou(false)
+      }
     //модальное окно изменения статуса заказа
     const [isModalStatus, setModalStatus] = React.useState(false)
     const onCloseStatus = () => {
-        setModalStatus(false)
+        setModalCart(false)
     }
     const [SelectedStatus, setSelectedStatus] = React.useState(false)
     const [SelectedIdEditOrderStatus, setSelectedIdEditOrderStatus] = React.useState(false)
@@ -76,7 +95,8 @@ const Home = observer(() => {
     const onAuth = () => {
         console.log("onAuth")
         UserStore.login(login, password)
-
+        CatalogStore.get()
+        OrdersStore.get()
     }
 
     //модальное окно регистрации
@@ -199,11 +219,11 @@ const Home = observer(() => {
             />
         </Row>
         <Row className={'p-3'}>
-            <Col>
+            <Col className={'col-3 '}>
             <h6>КАТАЛОГ</h6>
             </Col>
-            <Col className={'d-flex'}>
-                <div className={'mx-2 mt-1'}>Сортировать по:</div> 
+            <Col className={'col-8 d-flex'}>
+                <div className={'mx-4 mt-1'}>Сортировать по:</div> 
                 <Button  
                 className={' btn-sm' }
                 onClick={()=>{
@@ -226,12 +246,117 @@ const Home = observer(() => {
                 }}
                 >От А до Я</Button>
             </Col> 
+  
+            <Col >
+                <button type="button" style={{height:"40px", outline: "none", border: "none", backgroundColor: "white"}} 
+                    onClick={()=>{
+                        setModalCart(true)
+                    }}>
+
+                    <div style={{ marginLeft: "40px", width: "24px", background: "red", borderRadius: "50%", textAlign: "center"}}>{UserStore.cart.length}</div>
+                    <svg style={{marginTop: "-32px"}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-basket2-fill" viewBox="0 0 16 16">
+                    <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383L5.93 1.757zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0v-2zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0v-2zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1z"/>
+                    </svg>
+                </button>
+
+                <Authorization
+                    visible={isModalCart}
+                    title="Корзина"
+                    content={
+                    <>
+                        <table class="table table-sm" >
+                            <thead>
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col"></th>
+                                    <th scope="col">Цена</th>
+                                    <th scope="col">Количество</th>
+                                    <th scope="col">Всего</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {UserStore.cart.map((o)=> (
+                                <tr>
+                                    <td><img style={{height: "80px", width: "80px"}}  src={CatalogStore.getProduct(o.id).image} /></td>
+                                    <td>{CatalogStore.getProduct(o.id).title}</td>
+                                    <td>{CatalogStore.getProduct(o.id).price}</td>
+                                    <td>
+                                        <button id={o.id} type="button" class="btn btn-outline-secondary mx-1" style={{width: "29px", height: "30px", outline: "none", border: "none" }} 
+                                                onClick={(e)=>{
+                                                    UserStore.plusCountCart(e.target.id)
+                                                }}>
+                                                <svg style={{width: "20px", height: "20px", marginLeft: "-9px", marginTop: "-12px"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                                </svg>
+                                            </button>
+                                        {o.count}
+                                        <button id={o.id} type="button" class="btn btn-outline-secondary mx-1" style={{width: "29px", height: "30px", outline: "none", border: "none" }} 
+                                                onClick={(e)=>{
+                                                    UserStore.minusCountCart(e.target.id)
+                                                }}>
+                                               <svg style={{width: "20px", height: "20px", marginLeft: "-9px", marginTop: "-12px"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                                                </svg>
+                                        </button>
+                                    </td>
+                                    <td>{CatalogStore.getProduct(o.id).price * o.count}</td>
+                                    <td>
+                                        <div className="mx-2">
+                                            <button id={o._id} type="button" class="btn btn-outline-secondary" style={{width: "29px", height: "30px", outline: "none", border: "none" }} 
+                                                onClick={(e)=>{
+                                                    UserStore.deleteCart(e.target.id)
+                                                }}>
+                                                <svg  style={{width: "20px", height: "20px", marginLeft: "-9px", marginTop: "-12px"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
+                                                <path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/>
+                                                </svg>
+                                                
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                            <tr>
+                                <td></td><td></td><td></td>
+                                <td> Всего </td>
+                                <td>{UserStore.sumCart()}</td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        
+                        <input type="text" class="form-control mt-5" value={fioAddOrder} placeholder="ФИО получателя" onChange={(e)=>fioAddOrderSetValue(e.target.value)} />
+                        <input type="text" class="form-control mt-2" value={phoneAddOrder} onChange={(e)=>phoneAddOrderSetValue(e.target.value)} placeholder="Телефон получателя" />
+                        <input type="text" class="form-control mt-2" value={addressAddOrder} placeholder="Адрес" onChange={(e)=>addressAddOrderSetValue(e.target.value)} />
+                        <input type="text" class="form-control mt-2" value={commentAddOrder} onChange={(e)=>commentAddOrderSetValue(e.target.value)} placeholder="Введите комментарий к заказу" />
+                    </>
+                    }
+                    footer={<button className="btn btn-warning" onClick={()=>ArrangeOrder()}>Оформить заказ</button>}
+                    onClose={onCloseCart}
+                />
+                <Authorization
+                    visible={isModalThankYou}
+                    title="Заказ оформлен!"
+                    content={
+                    <>
+                        <div>Спасибо!</div>
+                        <div>Номер Вашего заказа: {OrderNumber}</div>
+                    </>
+                    }
+                    footer={<button className="btn btn-warning" onClick={()=>onCloseThankYou()}>Ок</button>}
+                    onClose={onCloseThankYou}
+                />
+            </Col>
         </Row>
         <Row className={'p-3'}>
             <Col>
                 <Groups onChange={onChange}></Groups>
             </Col>
             <Col className="col-lg-9" >
+                {console.log(toJS(UserStore.cart))}
                 <Catalog sort = {sortType} group = {sortGroup}></Catalog>
             </Col> 
         </Row>
